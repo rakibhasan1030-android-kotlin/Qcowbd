92da5e773ba389934dcd7b61110b74fbec828cfd
Rakib <hasan.rakib@datasoft-bd.com>
2023-06-22 18:11:10 +0600

    TopBar with Drawer implemented.

--- app/src/main/java/rakib/hasan/qcowbd/presentation/home/HomeScreen.kt
+++ app/src/main/java/rakib/hasan/qcowbd/presentation/home/HomeScreen.kt
@@ -1,26 +1,52 @@
 package rakib.hasan.qcowbd.presentation.home
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import rakib.hasan.qcowbd.R
 import rakib.hasan.qcowbd.app.components.appbar.AppBar
+import rakib.hasan.qcowbd.presentation.home.components.Boxes
 import rakib.hasan.qcowbd.presentation.home.components.HomeProductItem
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -32,16 +58,45 @@ fun HomeScreen(
     Scaffold(
         topBar = { AppBar(drawerState = drawerState) }
     ) {
+        val scrollState = rememberScrollState()
+
         val context = LocalContext.current
         val state = viewModel.state.value
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(it)) {
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                items(state.products) { product ->
-                    HomeProductItem(product = product, onItemClick = {
-                        Toast.makeText(context, product.title, Toast.LENGTH_SHORT).show()
-                    })
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+                .scrollable(state = scrollState, orientation = Orientation.Vertical)
+        ) {
+            Column(
+                modifier = Modifier
+            ) {
+                Boxes(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(250.dp)
+                            .background(Color.Black)
+                    )
+                    Box(
+                        modifier = Modifier
+                            .padding(horizontal = 24.dp)
+                            .fillMaxWidth()
+                            .height(100.dp)
+                            .background(Color.Transparent)
+                    ) {
+                        HeaderCard()
+                    }
+                }
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(state.products) { product ->
+                        HomeProductItem(product = product, onItemClick = {
+                            Toast.makeText(context, product.title, Toast.LENGTH_SHORT).show()
+                        })
+                    }
                 }
             }
             if (state.error.isNotBlank()) {
@@ -61,6 +116,64 @@ fun HomeScreen(
                         .align(
                             Alignment.Center
                         )
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun HeaderCard() {
+    Card(
+        modifier = Modifier
+            .wrapContentHeight(Alignment.CenterVertically),
+        elevation =CardDefaults.cardElevation(
+            0.5.dp
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            for (i in 0..2) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(100.dp)
+                        .weight(1f)
+                        .background(Color.White),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Image(
+                            painterResource(id = R.drawable.trolly),
+                            contentDescription = "Trolly",
+                            modifier = Modifier
+                                .height(40.dp)
+                                .width(40.dp),
+                        )
+                        Text(
+                            modifier = Modifier
+                                .padding(start = 8.dp, top = 4.dp, bottom = 0.dp, end = 8.dp),
+                            text = "অনলাইন পেমেন্ট",
+                            style = MaterialTheme.typography.titleSmall.copy(
+                                lineHeight = 14.sp,  // Here line height
+                            ),
+                            textAlign = TextAlign.Center,
+                            textDecoration = TextDecoration.None,
+                            fontSize = 12.sp
+                        )
+                    }
+
+                }
+                if (i != 2) Divider(
+                    modifier = Modifier
+                        .width(1.dp)
+                        .fillMaxHeight()
                 )
             }
         }
---
+++ app/src/main/java/rakib/hasan/qcowbd/presentation/home/components/OverlapScreen.kt
@@ -0,0 +1,125 @@
+package rakib.hasan.qcowbd.presentation.home.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import rakib.hasan.qcowbd.app.ui.theme.DividerColor
+import rakib.hasan.qcowbd.app.ui.theme.QcowbdTheme
+import rakib.hasan.qcowbd.app.ui.theme.Red100
+import kotlin.random.Random
+
+
+@Composable
+fun Boxes(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    Layout(
+        modifier = modifier,
+        content = content,
+    ) { measurables, constraints ->
+        val largeBox = measurables[0]
+        val smallBox = measurables[1]
+        val looseConstraints = constraints.copy(
+            minWidth = 0,
+            minHeight = 0,
+        )
+        val largePlaceable = largeBox.measure(looseConstraints)
+        val smallPlaceable = smallBox.measure(looseConstraints)
+        layout(
+            width = constraints.maxWidth,
+            height = largePlaceable.height + smallPlaceable.height / 2,
+        ) {
+            largePlaceable.placeRelative(
+                x = 0,
+                y = 0,
+            )
+            smallPlaceable.placeRelative(
+                x = (constraints.maxWidth - smallPlaceable.width) / 2,
+                y = largePlaceable.height - smallPlaceable.height / 2
+            )
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun BoxesPreview() {
+    QcowbdTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column {
+                Text(
+                    text = "Before",
+                    modifier = Modifier.padding(all = 16.dp)
+                )
+                Boxes(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Blue)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(128.dp)
+                            .background(Color.Black)
+                    )
+                    Box(
+                        modifier = Modifier
+                            .padding(horizontal = 24.dp)
+                            .fillMaxWidth()
+                            .height(64.dp)
+                            .background(Color.Transparent)
+                    ) {
+                        Card(
+                            modifier = Modifier,
+
+                            ) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                            ) {
+                                for (i in 1..3) {
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .height(100.dp)
+                                            .weight(1f)
+                                            .background(Color(Random.nextInt()))
+                                    )
+                                    if(i != 3)Divider(
+                                        modifier = Modifier
+                                            .padding(vertical = 8.dp, horizontal = 8.dp)
+                                            .background(Color(0xFFd1d3e0))
+                                            .width(1.dp)
+                                            .fillMaxHeight()
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+                Text(
+                    text = "After",
+                    modifier = Modifier.padding(all = 16.dp)
+                )
+            }
+        }
+    }
+}
